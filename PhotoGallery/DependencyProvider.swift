69b7ce9d488d9db6bc7fd1022ce930aff0c5d8d2
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-02-22 12:55:20 +0200

    Update DependencyProvider.swift

--- PhotoGallery/DependencyProvider.swift
+++ PhotoGallery/DependencyProvider.swift
@@ -4,19 +4,27 @@ import RealmSwift
 typealias DatabaseQueue = dispatch_queue_serial_t
 
 class DependencyProvider {
-    static var jsonDecoder: JSONDecoder {
+    private static var jsonDecoder: JSONDecoder {
         return JSONDecoder()
     }
     private static var userDefaults: UserDefaults {
         return UserDefaults()
     }
+    private static let databaseQueue =  DispatchQueue(label: "database") as! DatabaseQueue
+    private static var realm: Realm {
+        databaseQueue.sync {
+            return try! Realm()
+        }
+    }
+    
+    //MARK: Network
     private static var networkService: NetworkService {
         return NetworkService()
     }
-    private static let databaseQueue: DatabaseQueue = DispatchQueue(label: "database") as! DatabaseQueue
     private static var networkDataFetcher: NetworkDataFetcher {
         return NetworkDataFetcher(networkService: self.networkService, jsonDecoder: self.jsonDecoder)
     }
+    //MARK: Data Sources
     private static var localDataSource: LocalDataSourceProtocol {
         return LocalDataSource(databaseQueue: self.databaseQueue, userDefaults: self.userDefaults)
     }
@@ -30,10 +38,11 @@ class DependencyProvider {
             userDefaults: self.userDefaults
         )
     }
+    //MARK: ViewModels
     private static var photosViewModel: PhotosViewModel {
         return PhotosViewModel(dataSource: self.dataSource)
     }
-    
+    //MARK: Views
     static var photosViewController: UINavigationController {
         return UINavigationController(rootViewController: PhotosCollectionViewController(photosViewModel: photosViewModel))
     }
